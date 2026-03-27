e481a5460de26e75b648f4bb513056676eeed7b5
Muhammed Fatih Tadık <fatihtadk@gmail.com>
2026-03-27 14:41:08 +0300

    ktor api service classes created and dto classes added.

---
+++ composeApp/src/commonMain/kotlin/com/example/movieappkmm/data/remote/KtorApi.kt
@@ -0,0 +1,31 @@
+package com.example.movieappkmm.data.remote
+
+import com.example.movieappkmm.util.Constants.API_KEY
+import com.example.movieappkmm.util.Constants.BASE_URL
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.parameter
+import io.ktor.http.path
+import io.ktor.http.takeFrom
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+internal abstract class KtorApi {
+    val client = HttpClient {
+        install(ContentNegotiation){
+            json(Json {
+                ignoreUnknownKeys = true
+                useAlternativeNames = false
+            })
+        }
+    }
+
+    fun HttpRequestBuilder.pathUrl(path : String) {
+        url{
+            takeFrom(BASE_URL)
+            path("3",path)
+            parameter("api_key",API_KEY)
+        }
+    }
+
+}
---
+++ composeApp/src/commonMain/kotlin/com/example/movieappkmm/data/remote/MoviesResponse.kt
@@ -0,0 +1,5 @@
+package com.example.movieappkmm.data.remote
+
+internal data class MoviesResponse(
+    val results : List<MovieRemote>
+)
