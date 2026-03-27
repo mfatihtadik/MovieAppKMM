8c8aba2958c65ebd20b6ad3c3e21156107249281
Muhammed Fatih Tadık <fatihtadk@gmail.com>
2026-03-27 16:44:20 +0300

    repository classes created.

---
+++ composeApp/src/commonMain/kotlin/com/example/movieappkmm/domain/repository/MovieRepository.kt
@@ -0,0 +1,11 @@
+package com.example.movieappkmm.domain.repository
+
+import com.example.movieappkmm.domain.model.Movie
+
+internal interface MovieRepository {
+
+    suspend fun getMovies(page: Int) : List<Movie>
+
+    suspend fun getMovie(movieId: Int) : Movie
+
+}
